bbf4e279af700d27a37c59a4c2cfa1ca049f670f
Anurag Singh <anuragsinghofficial2611@gmail.com>
2026-06-14 11:34:23 +0530

    removed skills icon images and added icons from skillicons.dev

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,17 +1,4 @@
-const js = "images/languages/js.png"
-const html = "images/Web/html.png"
-const css = "images/Web/css.png"
-const react = "images/Web/react.png"
-const python = "images/languages/python.png"
-const cpp = "images/languages/cpp.png"
-const java = "images/languages/java.png"
-const sql = "images/DBs/mysql.png"
-const md = "images/Tools/markdown.png" // Corrected 'tools' to 'Tools'
-const mongodb = "images/DBs/mongodb.png"
-const git = "images/Tools/git.png"
-const vscode = "images/Tools/vscode.png"
-const figma = "images/Tools/figma.png"
-const vercel = "images/Tools/vercel.png"
+
 const antigravity = "images/Tools/antigravity.png"
 
 const Skills = () => {
@@ -19,31 +6,31 @@ const Skills = () => {
         <div className="skills-cont" id="skills">
             <div className="skills-title">What do I know ?</div>
             <div className="skill-row-1">
-                <div className="skill-js" id="skill"><img src={js} alt="js" /><br /><hr />Intermediate</div>
-                <div className="skill-java" id="skill"><img src={java} alt="java" /><br /><hr />Intermediate</div>
-                <div className="skill-py" id="skill"><img src={python} alt="python" /><br /><hr />Beginner</div>
-                <div className="skill-cpp" id="skill"><img src={cpp} alt="cpp" /><br /><hr />Intermediate</div>
+                <div className="skill-js" id="skill"><img src="https://skillicons.dev/icons?i=js" alt="js" /><br /><hr />Intermediate</div>
+                <div className="skill-java" id="skill"><img src="https://skillicons.dev/icons?i=java" alt="java" /><br /><hr />Intermediate</div>
+                <div className="skill-py" id="skill"><img src="https://skillicons.dev/icons?i=python" alt="python" /><br /><hr />Beginner</div>
+                <div className="skill-cpp" id="skill"><img src="https://skillicons.dev/icons?i=cpp" alt="cpp" /><br /><hr />Intermediate</div>
                 <div></div>
                 <p className="lang">Languages</p>
             </div>
             <div className="skill-row-2">
-                <div className="skill-html" id="skill"><img src={html} alt="html" /><br /><hr />Intermediate</div>
-                <div className="skill-css" id="skill"><img src={css} alt="css" /><br /><hr />Intermediate</div>
-                <div className="skill-react" id="skill"><img src={react} alt="react" /><br /><hr />Intermediate</div>
+                <div className="skill-html" id="skill"><img src="https://skillicons.dev/icons?i=html" alt="html" /><br /><hr />Intermediate</div>
+                <div className="skill-css" id="skill"><img src="https://skillicons.dev/icons?i=css" alt="css" /><br /><hr />Intermediate</div>
+                <div className="skill-react" id="skill"><img src="https://skillicons.dev/icons?i=react" alt="react" /><br /><hr />Intermediate</div>
                 <div></div>
                 <p className="lang">Web Dev</p>
             </div>
             <div className="skill-row-3">
-                <div className="skill-sql" id="skill"><img src={sql} alt="PostgresSql" /><br /><hr />Intermediate</div>
-                <div className="skill-mongodb" id="skill"><img src={mongodb} alt="mongodb" /><br /><hr />Beginner</div>
+                <div className="skill-sql" id="skill"><img src="https://skillicons.dev/icons?i=mysql" alt="PostgresSql" /><br /><hr />Intermediate</div>
+                <div className="skill-mongodb" id="skill"><img src="https://skillicons.dev/icons?i=mongodb" alt="mongodb" /><br /><hr />Beginner</div>
                 <div></div>
                 <p className="lang">Databases</p>
             </div>
             <div className="skill-row-4">
-                <div className="skill-git" id="skill"><img src={git} alt="Git version control" /><br /><hr />Version Control</div>
-                <div className="skills-vscode" id="skill"><img src={vscode} alt="VS Code" /><br /><hr />Editor</div>
+                <div className="skill-git" id="skill"><img src="https://skillicons.dev/icons?i=git" alt="Git version control" /><br /><hr />Version Control</div>
+                <div className="skills-vscode" id="skill"><img src="https://skillicons.dev/icons?i=vscode" alt="VS Code" /><br /><hr />Editor</div>
                 <div className="skills-antigravity" id="skill"><img src={antigravity} alt="Antigravity" /><br /><hr />Editor</div>
-                <div className="skill-md" id="skill"><img src={md} alt="Markdown" /><br /><hr />Writting</div>
+                <div className="skill-md" id="skill"><img src="https://skillicons.dev/icons?i=markdown" alt="Markdown" /><br /><hr />Writting</div>
                 <div></div>
                 <p className="lang">Tools</p>
             </div>
